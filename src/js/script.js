9a67118e99f3bba06129eebbf6197a9d854ee175
Nicholas Neutron <nicholasneutron2277@gmail.com>
2021-01-24 17:22:49 +0300

    implemented minimax ai logic

--- src/js/script.js
+++ src/js/script.js
@@ -72,6 +72,10 @@ const Renderer = (() => {
     return document.querySelectorAll('.square');
   };
 
+  const getSquare = (index) => {
+    return document.querySelectorAll('.square')[index];
+  };
+
   const setSquareMark = (mark) => {
     return `<span>${mark}</span>`;
   };
@@ -102,6 +106,22 @@ const Renderer = (() => {
     document.querySelector('.game-result').textContent = '';
   };
 
+  const getPlayerOneType = () => {
+    return dropdowns[0].firstChild.textContent;
+  };
+
+  const getPlayerTwoType = () => {
+    return dropdowns[1].firstChild.textContent;
+  };
+
+  const renderAiTurn = (index, marker, color) => {
+    selectedSquare = getSquare(index);
+    selectedSquare.textContent = marker;
+    selectedSquare.style.color = color;
+    selectedSquare.classList.remove('opaque');
+    return selectedSquare;
+  };
+
   return {
     renderBoard,
     getSquares,
@@ -115,6 +135,9 @@ const Renderer = (() => {
     hideMainMenu,
     showGame,
     hideGame,
+    renderAiTurn,
+    getPlayerOneType,
+    getPlayerTwoType,
   };
 })();
 
@@ -140,7 +163,7 @@ const Gameboard = (() => {
 
   const reset = () => {
     board.length = 0;
-  }
+  };
 
   const setMarker = (marker, index) => {
     board[index] = { marker };
@@ -194,6 +217,76 @@ const Gameboard = (() => {
     return board.every((square) => square.marker !== '') ? true : false;
   };
 
+  const checkEmpty = (index) => {
+    return board[index].marker === '' ? true : false;
+  };
+
+  const checkAiWin = () => {
+    if (checkRows('O') || checkColumns('O') || checkDiagonals('O')) {
+      return 10;
+    } else {
+      return -10;
+    }
+  };
+
+  const minimax = (isMax) => {
+    let score = checkAiWin();
+
+    if (score === 10) {
+      return 10;
+    }
+
+    if (score === -10) {
+      return -10;
+    }
+
+    if (checkDraw()) {
+      return 0;
+    }
+
+    if (isMax) {
+      let best = -1000;
+      for (let i = 0; i < board.length; i++) {
+        if (checkEmpty(i)) {
+          board[i].marker = 'O';
+          best = Math.max(best, minimax(board, !isMax));
+          board[i].marker = '';
+        }
+      }
+      return best;
+    } else {
+      best = 1000;
+      for (let i = 0; i < board.length; i++) {
+        if (checkEmpty(i)) {
+          board[i].marker = 'X';
+          best = Math.min(best, minimax(board, !isMax));
+          baoard[i].marker = '';
+        }
+      }
+      return best;
+    }
+  };
+
+  const bestMove = () => {
+    let bestValue = -1000;
+    let moveIndex = -1;
+
+    for (let i = 0; i < board.length; i++) {
+      if (checkEmpty(i)) {
+        board[i].marker = 'O';
+        let moveValue = minimax(board, false);
+        console.warn(moveValue);
+        board[i].marker = '';
+
+        if (moveValue > bestValue) {
+          moveIndex = i;
+          bestValue = moveValue;
+        }
+      }
+    }
+    return moveIndex;
+  };
+
   return {
     getBoard,
     init,
@@ -204,28 +297,35 @@ const Gameboard = (() => {
     checkDraw,
     clear,
     reset,
+    bestMove,
   };
 })();
 
-const Player = (name, marker, turn) => {
+const Player = (name, marker, turn, type, color) => {
   const getName = () => name;
   const getMarker = () => marker;
   const getTurn = () => turn;
   const toggleTurn = () => (turn = !turn);
+  const getType = () => type;
+  const getColor = () => color;
 
   return {
     getName,
     getMarker,
     getTurn,
     toggleTurn,
+    getType,
+    getColor,
   };
 };
 
 const Controller = (() => {
-  const playerOne = Player('one', 'X', true);
-  const playerTwo = Player('two', 'O', false);
+  let playerOne;
+  let playerTwo;
   const playButton = document.querySelector('.btn-play');
   playButton.addEventListener('click', () => {
+    playerOne = Player('Player 1', 'X', true, Renderer.getPlayerOneType(), 'rgba(66, 102, 150, 0.8)');
+    playerTwo = Player('Player 2', 'O', false, Renderer.getPlayerTwoType(), 'rgba(248, 82, 82, 0.8)');
     Renderer.hideMainMenu();
     Renderer.showGame();
     init();
@@ -270,17 +370,16 @@ const Controller = (() => {
   const onGameFinished = (result) => {
     Renderer.displayResult(result);
     Renderer.displayControls();
-    Renderer.getSquares().forEach((square) => square.removeEventListener('click', gameHandler));
+    Renderer.getSquares().forEach((square) => square.removeEventListener('click', gameHandlerTwoHumans));
   };
 
-  const gameHandler = (event) => {
+  const gameHandlerTwoHumans = (event) => {
     const index = Array.from(event.currentTarget.parentNode.children).indexOf(event.currentTarget);
-
     if (playerOne.getTurn()) {
-      event.currentTarget.style.color = 'rgba(66, 102, 150, 0.8)';
+      event.currentTarget.style.color = playerOne.getColor();
       event.currentTarget.textContent = playerOne.getMarker();
       event.currentTarget.classList.remove('opaque');
-      event.currentTarget.removeEventListener('click', gameHandler);
+      event.currentTarget.removeEventListener('click', gameHandlerTwoHumans);
 
       Gameboard.setMarker(playerOne.getMarker(), index);
       if (checkWin(playerOne.getMarker())) {
@@ -290,13 +389,12 @@ const Controller = (() => {
         onGameFinished("It's a Draw!");
         return;
       }
-
       changeTurns();
     } else if (playerTwo.getTurn()) {
-      event.currentTarget.style.color = 'rgba(248, 82, 82, 0.8)';
+      event.currentTarget.style.color = playerTwo.getColor();
       event.currentTarget.textContent = playerTwo.getMarker();
       event.currentTarget.classList.remove('opaque');
-      event.currentTarget.removeEventListener('click', gameHandler);
+      event.currentTarget.removeEventListener('click', gameHandlerTwoHumans);
 
       Gameboard.setMarker(playerTwo.getMarker(), index);
       if (checkWin(playerTwo.getMarker())) {
@@ -312,10 +410,18 @@ const Controller = (() => {
       return;
     }
   };
+
   const gameloop = () => {
-    Renderer.getSquares().forEach((square) => {
-      square.addEventListener('click', gameHandler);
-    });
+    if (playerOne.getType() === 'Human' && playerTwo.getType() === 'Human') {
+      Renderer.getSquares().forEach((square) => {
+        square.addEventListener('click', gameHandlerTwoHumans);
+      });
+    } else if (
+      (playerOne.getType() === 'AI' && playerTwo.getType() === 'Human') ||
+      (playerOne.getType() === 'Human' && playerTwo.getType() === 'AI')
+    ) {
+      //to do
+    }
   };
 
   const init = () => {
